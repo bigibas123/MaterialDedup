a775b844b7d9491edaa9d18a14b1271f4c8160b5
Bas Dingemans <bas@dingemans.cc>
2024-10-18 22:49:12 +0200

    Slightly split up big method chain

--- Editor/VRCAvatarDescriptorExtensions.cs
+++ Editor/VRCAvatarDescriptorExtensions.cs
@@ -9,7 +9,8 @@ namespace cc.dingemans.bigibas123.MaterialDedup.Editor.Animation
 {
 	public static class VRCAvatarDescriptorExtensions
 	{
-		public static IEnumerable<MaterialTarget> GetMaterialTargetFromAnimationLayers(this VRCAvatarDescriptor descriptor)
+		public static IEnumerable<MaterialTarget> GetMaterialTargetFromAnimationLayers(
+			this VRCAvatarDescriptor descriptor)
 		{
 			return descriptor?.baseAnimationLayers?.GetAnimationControllers()
 				.Concat(descriptor?.specialAnimationLayers?.GetAnimationControllers())
@@ -22,13 +23,19 @@ namespace cc.dingemans.bigibas123.MaterialDedup.Editor.Animation
 					.SelectMany(clip => AnimationUtility.GetObjectReferenceCurveBindings(clip)
 						.Where(binding => binding.type.IsTargetTypeSupported())
 						.Where(binding => binding.propertyName.StartsWith("m_Materials.Array"))
-						.SelectMany(binding => AnimationUtility.GetObjectReferenceCurve(clip, binding)
-							.Select(
-								(_, i) => new AnimationMaterialTarget(clip, binding, i)
-							)
+						.SelectMany(binding => binding.GetMaterialsFromBinding(clip)
 						)
 					)
 				;
+		}
+
+		public static IEnumerable<MaterialTarget> GetMaterialsFromBinding(this EditorCurveBinding binding,
+			AnimationClip clip)
+		{
+			return AnimationUtility.GetObjectReferenceCurve(clip, binding)
+				.Select(
+					(_, i) => new AnimationMaterialTarget(clip, binding, i)
+				);
 		}
 
 		public static IEnumerable<RuntimeAnimatorController> GetAnimationControllers(
@@ -46,6 +53,5 @@ namespace cc.dingemans.bigibas123.MaterialDedup.Editor.Animation
 
 			return controllers;
 		}
-		
 	}
 }
